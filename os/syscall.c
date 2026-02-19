3baeb59f8d2b8cc2e34259488622fba8f436f6cb
Joshua Patrick <129539757+Jpatrick2@users.noreply.github.com>
2026-02-19 16:24:33 +0000

    Chapter 3 Submission

--- os/syscall.c
+++ os/syscall.c
@@ -40,6 +40,43 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 * LAB1: you may need to define sys_task_info here
 */
 
+int sys_task_info(struct task_info *info) {
+    if (!info) return -1;
+
+    struct proc *cur = curr_proc();
+
+    switch (cur->state) {
+    case RUNNABLE:
+        info->status = Ready;
+        break;
+    case RUNNING:
+        info->status = Running;
+        break;
+    case UNUSED:
+    case USED:
+        info->status = UnInit;
+        break;
+    case ZOMBIE:
+    case SLEEPING:
+    default:
+        info->status = Exited;
+        break;
+    }
+
+    for (int i = 0; i < MAX_SYSCALL_NUM; i++) {
+        info->syscall_times[i] = cur->syscall_times[i]; 
+    }
+
+    uint64 cycle = get_cycle();
+    uint64 sec  = cycle / CPU_FREQ;
+    uint64 usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
+    uint64 ms   = sec * 1000 + usec / 1000;
+
+    info->time = (int)ms;   
+
+    return 0;
+}
+
 extern char trap_page[];
 
 void syscall()
@@ -53,6 +90,13 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	struct proc *cur = curr_proc();
+
+	cur->syscall_count++;
+	if (id < MAX_SYSCALL_NUM) {
+		cur->syscall_times[id]++;  
+	}
+
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -69,6 +113,12 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	case SYS_getpid:
+    	ret = curr_proc()->pid;
+    	break;
+	case SYS_task_info:
+		ret = sys_task_info((struct task_info *)args[0]);
+    	break;
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
